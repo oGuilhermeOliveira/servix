f86c2b4078b261ad1653c166a16fa33013fa7c51
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2026-05-28 18:14:24 -0300

    firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Preencha com os dados reais do seu projeto Firebase.
+// Este arquivo esta no .gitignore para evitar versionar credenciais.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6rCYFytmrIHO0HRxfGabgyz-vbNhcLbk",
+  authDomain: "bancoservix.firebaseapp.com",
+  projectId: "bancoservix",
+  storageBucket: "bancoservix.firebasestorage.app",
+  messagingSenderId: "977889290545",
+  appId: "1:977889290545:web:f247f1217a24da85736810",
+  measurementId: "G-1ZQDCNKT9E"
+};
